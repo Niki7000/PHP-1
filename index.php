178d43247e65b40ded487cb67c6e4476ec2b7657
Niki7000 <petefi.nikodingregorin@gmail.com>
2025-10-22 22:38:05 +0200

    Ispitivanje punoletstva i dodavanje razmaka

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
     $visina = 182.6; //Ovo je float
     $programer = true; //Ovo je bool
 
-    echo "Moje ime je $ime, i imam $godine godina, visok sam $visina. ";
+    echo "Moje ime je $ime, i imam $godine godina, visok sam $visina. <br>";
     $ime = "Niki";
     $godine = 19;
-    echo "Moje ime je $ime, i imam $godine godina, visok sam $visina.";
+    echo "Moje ime je $ime, i imam $godine godina, visok sam $visina. <br>";
 
+    if($godine >= 18)
+    {
+        echo "Punoletni ste.<br>";
+    }
+    else
+    {
+        echo "Maloletni ste.<br>";
+    }
 ?>
